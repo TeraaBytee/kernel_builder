cfdbf031fc3ce8c8980bd8ad73195555665d2c9a
A. YulianT <85273605+TeraaBytee@users.noreply.github.com>
2021-11-25 12:11:40 +0700

    Update build_gk.sh

--- build_gk.sh
+++ build_gk.sh
@@ -40,7 +40,7 @@ ZIP_KERNEL_VERSION="4.14.$(cat "$MainPath/Makefile" | grep "SUBLEVEL =" | sed 's
 TIME=$(date +"%m%d%H%M")
 
 # Start building
-Compiler=GK
+Compiler=GengKapak
 MAKE="./makeparallel"
 rm -rf out
 BUILD_START=$(date +"%s")
